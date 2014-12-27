7a5ec0d0ca5fa2593d07b3d5051feefdcb3d769f
Christopher Tarwater <ctarwater@gmail.com>
2014-12-26 17:44:49 -0800

    update jekyll-minimagick plugin

--- _plugins/mini_magick.rb
+++ _plugins/mini_magick.rb
@@ -44,8 +44,10 @@ module Jekyll
 
         FileUtils.mkdir_p(File.dirname(dest_path))
         image = ::MiniMagick::Image.open(path)
-        @commands.each_pair do |command, arg|
-          image.send command, arg
+        image.combine_options do |c|
+          @commands.each_pair do |command, arg|
+            c.send command, arg
+          end
         end
         image.write dest_path
 
@@ -63,7 +65,7 @@ module Jekyll
       def generate(site)
         return unless site.config['mini_magick']
 
-        site.config['mini_magick'].each_pair do |name, preset|
+         site.config['mini_magick'].each_pair do |name, preset|
           Dir.glob(File.join(site.source, preset['source'], "*.{png,jpg,jpeg,gif}")) do |source|
             site.static_files << GeneratedImageFile.new(site, site.source, preset['destination'], File.basename(source), preset.clone)
           end
